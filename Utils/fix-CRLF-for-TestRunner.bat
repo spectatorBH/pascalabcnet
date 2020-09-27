722b99f837389158faf59d820c20aebc24b35673
spectatorBH <spectator2020@inbox.lv>
2020-09-27 04:02:56 +0300

    Custom CR/LF script fails only under VM
    Disabled it, git autoCRLF config seems to work fine anyway.

--- Utils/fix-CRLF-for-TestRunner.bat
+++ Utils/fix-CRLF-for-TestRunner.bat
@@ -2,19 +2,13 @@
 ::#  Written by spectatorBH for PascalABC.NET project. Published as public domain, Sep-2020  #
 ::============================================================================================
 ::
-@echo on
+@echo off
 SETLOCAL
 echo.
 echo [%~nx0] ------ SCRIPT STARTED -------
 echo.
 echo [INFO] Converting line-endings in all '*.pas' files under 'TestSuite\formatter_tests' to CR/LF standard:
-findstr /e /i "VBS-line" "%~f0" 
-@rem > "%temp%\~rewrite-lines.vbs" || goto ERROR
-@rem set _err_code=%ERRORLEVEL% & echo ERRORLEVEL = %ERRORLEVEL%
-@rem dir "%temp%"
-@rem type "%temp%\~rewrite-lines.vbs"
-@rem if %_err_code% NEQ 0 goto ERROR
-if %errorlevel% NEQ 0 goto ERROR
+findstr /e /i "VBS-line" "%~0" > "%temp%\~rewrite-lines.vbs" || goto ERROR
 
 set "_dir=%~dp0..\TestSuite\formatter_tests"
 forfiles /m *.pas /s /p "%_dir%" /c "cmd /c echo Rewriting -- @path && cscript //NoLogo """%temp%\~rewrite-lines.vbs""" <@path >@path.CRLF && move /Y @path.CRLF @path 1>nul"
